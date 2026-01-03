6a51ac8ba05c9ce1ef688f20699a46a82ce3b631
24p65a0526 <24p65a0526@vbithyd.ac.in>
2026-01-03 17:05:47 +0530

    Add Test.js with greeting and arithmetic functions

---
+++ Test.js
@@ -0,0 +1,15 @@
+console.log("This is a test file.");
+function greet() {
+    console.log("Hello, World!");
+
+}
+function add(a, b) {
+    return a + b;
+}
+function farewell() {
+    console.log("Goodbye, World!");
+
+}
+function additionalFunction() {
+    console.log("This is an additional function.");
+}
